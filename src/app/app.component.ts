778bedf2e0b984dc88557e5ccfeaa68804d713fb
mariohalim00 <mariohalim00@gmail.com>
2023-02-10 21:42:23 +0700

    practice angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learning_angular';
+  title = 'Learning Angular';
+  counter:string = 'its going';
+
+  incrementCounter(){
+    this.counter += ' and going...'
+  }
 }
